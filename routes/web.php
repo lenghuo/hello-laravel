32d1253f4e46e81ae9d13fe383cae98e5492b2ce
lenghuo <weixupeng999@163.com>
2017-09-13 15:54:45 +0000

    finish static pages

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home','StaticPagesController@home');
+Route::get('/help','StaticPagesController@help');
+Route::get('/about','StaticPagesController@about');
